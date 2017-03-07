5a0f331d5567d30a93b8c366e20a4ec9b8f30d4b
Lily Beauvilliers <D16125346@mydit.ie>
2017-03-07 18:50:21 +0000

    Validation as Maven project with Int validation working

--- Validation2/src/main/java/BaseDataIntValidation.java
+++ Validation2/src/main/java/BaseDataIntValidation.java
@@ -1,51 +1,64 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import com.sun.xml.internal.ws.util.StringUtils;
-import org.apache.commons.lang.math.NumberUtils;
+import java.io.IOException;
+import org.apache.commons.lang3.math.NumberUtils;
 
 public class BaseDataIntValidation {
+	
 	//importing file"s"(as needed)
-	String baseDataFile = "Z:/Masters/JEECourse/CellExtraction/Test2Data.csv";
+	String baseDataFile = "C:/group-repo/group-proj-repo/TestExcelManip/upload/base_data.txt";
 	String line = "";
     String splitBy = ";";
+    
+    //constructor
+    public BaseDataIntValidation(){
+    
+    	intValidation();
+    }
+    
+    //validation method
+    public void intValidation(){
+    	
+    	try (BufferedReader br = new BufferedReader(new FileReader(baseDataFile))) {
+         	
+    		 //iteration variable removes the headings from the data file
+    		int iteration = 0;
+         	
+    		while ((line = br.readLine()) != null) {
+    			
+             	if (iteration == 0){
+             		iteration ++;
+             			continue;
+             	} //endif
+             	
+             // use comma as separator to read fields in data line
+             	String[] currentLine = line.split(splitBy);
+             	
+                //fields to be validated
+                String eventID = currentLine[1];
+                String failureClass = currentLine[2];
+                String ueType = currentLine[3];
+                String market = currentLine[4];
+                String operator = currentLine[5];
+                String cellID = currentLine[6];
+                String duration = currentLine[7];
+                String causeCode = currentLine[8];
+                String imsi = currentLine[10];
+                
+                //Checking eventID for any values not of type int
+                System.out.println(eventID);
+                System.out.println(NumberUtils.isDigits(eventID));
+    		
+    		} //endwhile
+    	} catch (IOException e) {
+             e.printStackTrace();
+        }
+}
 	
-
-	public BaseDataIntValidation() {
-		
-		public  void intValidation(){
-			try (BufferedReader br = new BufferedReader(new FileReader(baseDataFile))) {
-	         	
-				 //iteration variable removes the headings from the data file
-				int iteration = 0;
-	         	
-				while ((line = br.readLine()) != null) {
-					
-	             	if (iteration == 0){
-	             		iteration ++;
-	             			continue;
-	             	}
-	             // use comma as separator to read fields in data line
-                    String[] BaseDataSheet = line.split(splitBy);
-                    //fields to be validated
-                    String eventID = BaseDataSheet[1];
-                    String failureClass = BaseDataSheet[2];
-                    String ueType = BaseDataSheet[3];
-                    String market = BaseDataSheet[4];
-                    String operator = BaseDataSheet[5];
-                    String cellID = BaseDataSheet[6];
-                    String duration = BaseDataSheet[7];
-                    String causeCode = BaseDataSheet[8];
-                    String imsi = BaseDataSheet[10];
-                    
-                    //Checking eventID for any values not of type int
-                    NumberUtils.isCreatable(eventID);
-				
-                    {
-                       // Number is integer
-                    }
-				}
-			} 			
+	//main method
+		public static void main(String[] args) {
+			new BaseDataIntValidation();
 		}
-	}
+}
             			
 	             			
