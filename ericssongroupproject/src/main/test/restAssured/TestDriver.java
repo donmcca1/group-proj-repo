4c4dbf9c71adeac9779c8b717dec3077cf45636b
donmcca1 <donmcca1@gmail.com>
2017-04-21 15:27:39 +0100

    arquillian tests are now running on remote container instead of managed

--- ericssongroupproject/src/main/test/restAssured/TestDriver.java
+++ ericssongroupproject/src/main/test/restAssured/TestDriver.java
@@ -15,7 +15,7 @@ public class TestDriver {
     public static void setup() {
         String port = System.getProperty("server.port");
         if (port == null) {
-            RestAssured.port = Integer.valueOf(8080);
+            RestAssured.port = Integer.valueOf(9090);
         }
         else{
             RestAssured.port = Integer.valueOf(port);
